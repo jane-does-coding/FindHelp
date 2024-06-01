93205100fbc9c686cd2838f4d8c22a8c4da4810b
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-01 09:58:44 -0600

    OMG I THINK IT WORKS

--- app/components/Feed/Posts.tsx
+++ app/components/Feed/Posts.tsx
@@ -1,10 +1,12 @@
 import { Post as PostTypeMaybe } from "@prisma/client";
 import Post from "./Post";
 
-const Posts = ({ posts }: { posts: PostTypeMaybe[] }) => {
+const Posts = ({ posts }: { posts: PostTypeMaybe[] | null }) => {
+	if (!posts) return <h1>hello</h1>;
+
 	return (
 		<div className="flex flex-col gap-3 p-4 h-screen overflow-auto w-full">
-			{posts.map((post: any) => (
+			{posts.map((post: PostTypeMaybe) => (
 				<div key={post.id}>
 					<Post post={post} />
 				</div>
