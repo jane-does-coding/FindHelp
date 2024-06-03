41458017ef43d99cee6347472c07b8dd33f826a7
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-06-03 10:51:40 -0600

    my posts page fixed

--- app/(pages)/my_posts/page.tsx
+++ app/(pages)/my_posts/page.tsx
@@ -6,7 +6,15 @@ import React from "react";
 const page = async () => {
 	const posts: Post[] | null = await getUserPosts();
 
-	if (!posts) return <h1>nothing</h1>;
+	if (!posts || posts.length == 0) {
+		return (
+			<div className="md:ml-[20vw] text-white bg-black/25 w-[100vw] md:w-[80vw] h-screen">
+				<h1 className="text-5xl py-10 text-center jura text-white">
+					You have no posts
+				</h1>
+			</div>
+		);
+	}
 
 	return (
 		<div className="md:ml-[20vw] text-white bg-black/25 w-[100vw] md:w-[80vw] h-screen">
